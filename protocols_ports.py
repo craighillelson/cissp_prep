a212de92af31660ccc2346ca7128340913629f3c
Craig Hillelson <craig.hillelson@intersection.com>
2018-11-07 07:53:29 -0500

    refactored to conform to pep8 conventions

--- protocols_ports.py
+++ protocols_ports.py
@@ -6,39 +6,46 @@ print("protocols and ports".upper())
 
 # build dictionary
 protocols_ports_dict = {
-	'FTP': '21',
-	'SSH': '22',
-	'Telnet': '23',
-	'SMTP': '25',
-	'DNS': '53',
-	'TFTP': '69',
-	'HTTP': '80',
-	'POP3': '110',
-	'NTP': '123',
-	'SSL': '443',
-	'LPD': '515',
-	'Microsoft SQL Server': '1433',
-	'Oracle': '1521',
-	'H.323': '1720',
-	'RDP': '3389',
-}
+    'FTP': '21',
+    'SSH': '22',
+    'Telnet': '23',
+    'SMTP': '25',
+    'DNS': '53',
+    'TFTP': '69',
+    'HTTP': '80',
+    'POP3': '110',
+    'NTP': '123',
+    'SSL': '443',
+    'LPD': '515',
+    'Microsoft SQL Server': '1433',
+    'Oracle': '1521',
+    'H.323': '1720',
+    'PPTP': '1723',
+    'RDP': '3389',
+    }
 
 # create list to be populated later
 incorrects = []
 
 # quiz user
 for k, v in protocols_ports_dict.items():
-	print("protocol %s") % (k)
-	port_answer = raw_input("port ")
-	if port_answer == v:
-		print("correct")
-	else:
-		print("incorrect")
-		print("the correct answer is %s") % (v)
-		incorrects.append(k)
-	print(rtn())
+    print("protocol %s") % (k)
+    port_answer = raw_input("port ")
+    if port_answer == v:
+        print("correct")
+    else:
+        print("incorrect")
+        print("the correct answer is %s") % (v)
+        incorrects.append(k)
+    print(rtn())
 
-# show user what needs review
-print("items for review".upper())
-for protocol in incorrects:
-	print(protocol)
+# results - show user what needs review
+if len(incorrects) > 0:
+    print("items for review".upper())
+    for protocol in incorrects:
+        print(protocol)
+else:
+    print("100%! Great job!!!")
+
+# for readability
+print(rtn())
