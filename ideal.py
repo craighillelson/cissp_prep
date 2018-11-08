835b00161b7389b556fab5e7e7161a88f4c53f98
Craig Hillelson <craig.hillelson@intersection.com>
2018-11-08 07:34:35 -0500

    removed obvious comment

---
+++ ideal.py
@@ -0,0 +1,24 @@
+# define lambda
+rtn = lambda: '\n'
+
+# define ideal list
+ideal = [
+	'initiating',
+	'diagnosing',
+	'establishing',
+	'acting',
+	'learning',
+	]
+
+i = 0
+
+# quiz user
+for item in ideal:
+	prompt_user = raw_input("name one of the elements ")
+	if prompt_user == ideal[i]:
+		print("correct")
+		print(rtn())
+	else:
+		print("Incorrect. The correct answer is %s") % (i)
+		print(rtn())
+	i = i + 1
