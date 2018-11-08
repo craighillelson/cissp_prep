bb53b5a8a079aeed63a24b16a7bc297bd2abaa95
Craig Hillelson <craig.hillelson@intersection.com>
2018-11-08 07:34:06 -0500

    added quiz function

---
+++ swcmm.py
@@ -0,0 +1,24 @@
+# for readability
+rtn = lambda: '\n'
+
+# swcmm dictionary
+swcmm = [
+	'initial',
+	'repeatable',
+	'defined',
+	'managed',
+	'optimized',
+	]
+
+i = 0
+
+# quiz user
+for item in swcmm:
+	prompt_user = raw_input("name one of the elements ")
+	if prompt_user == swcmm[i]:
+		print("correct")
+		print(rtn())
+	else:
+		print("Incorrect. The correct answer is %s") % (i)
+		print(rtn())
+	i = i + 1
