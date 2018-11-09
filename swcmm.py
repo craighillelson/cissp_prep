c2e74020a8fa33c821dab59ed81de842a557e245
Craig Hillelson <craig.hillelson@intersection.com>
2018-11-09 05:13:32 -0500

    formatted to conform to pep8 standards

--- swcmm.py
+++ swcmm.py
@@ -3,22 +3,22 @@ rtn = lambda: '\n'
 
 # swcmm dictionary
 swcmm = [
-	'initial',
-	'repeatable',
-	'defined',
-	'managed',
-	'optimized',
-	]
+    'initial',
+    'repeatable',
+    'defined',
+    'managed',
+    'optimized',
+    ]
 
 i = 0
 
 # quiz user
 for item in swcmm:
-	prompt_user = raw_input("name one of the elements ")
-	if prompt_user == swcmm[i]:
-		print("correct")
-		print(rtn())
-	else:
-		print("Incorrect. The correct answer is %s") % (swcmm[i])
-		print(rtn())
-	i = i + 1
+    prompt_user = raw_input("name one of the elements ")
+    if prompt_user == swcmm[i]:
+        print("correct")
+        print(rtn())
+    else:
+        print("Incorrect. The correct answer is %s") % (swcmm[i])
+        print(rtn())
+    i = i + 1
