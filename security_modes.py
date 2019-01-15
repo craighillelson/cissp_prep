6a4c0f2f8984f285cb08fc7b19a54164cd833645
Craig Hillelson <craig.hillelson@intersection.com>
2019-01-14 22:30:45 -0500

    refactored in response to pylint findings

--- security_modes.py
+++ security_modes.py
@@ -42,30 +42,3 @@ for k, v in SECURITY_MODES.items():
     print "%s" % (k)
     print "%s, %s, %s" % (v[0], v[1], v[2])
     print RTN()
-    '''
-    clearance = raw_input("clearance ")
-    if clearance == v[0]:
-        need_to_know = raw_input("need to know ")
-        if need_to_know == v[1]:
-            pdmcl = raw_input("pdmcl ")
-            if pdmcl == v[2]:
-                print(rtn())
-            else:
-                print("incorrect %s") % (v[1])
-        else:
-            print("incorrect %s") % (v[2])
-    else:
-        print("incorrect %s") % (v[0])
-        need_to_know = raw_input("need to know ")
-        if need_to_know == v[1]:
-            pdmcl = raw_input("pdmcl ")
-            if pdmcl == v[2]:
-                print(rtn())
-            else:
-                print("incorrect %s") % (v[1])
-        else:
-            print("incorrect %s") % (v[2])
-    print(rtn())
-    '''
-
-# print(rtn())
