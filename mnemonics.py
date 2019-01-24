037caaa8ea8543b9b3876422ad08c187f5e6016a
Craig Hillelson <craighillelson@gmail.com>
2019-01-23 19:07:39 -0500

    create mnemonics.py

---
+++ mnemonics.py
@@ -0,0 +1,27 @@
+""" __doc__ """
+
+MNEMONICS = {
+    'Please Do Not Teach Surly People Acronyms': [
+        'physical', 
+        'data-link', 
+        'network', 
+        'transport', 
+        'session', 
+        'presentation', 
+        'application',
+        ],
+    'All Presidents Since Truman Never Did Pot': [
+        'application',
+        'session',
+        'transport',
+        'network',
+        'data-link',
+        'physical',
+        ],
+    'U S Can Stop Terrorism': [
+        'unclassified',
+        'sensitive',
+        'classified',
+        'secret',
+        'top secret',
+        ],
